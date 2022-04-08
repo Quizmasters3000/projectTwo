9c007a617aae8d256a16949226b8f46dea194ae4
Stephen Korzenstein <skorzenstein@gmail.com>
2022-04-07 23:08:11 -0400

    updated media queries

--- script/main.js
+++ script/main.js
@@ -54,12 +54,14 @@ quizApp.changeColor = function () {
   console.log(quizApp.gradient);
 };
 
+// Creates random number
 quizApp.getRandomNumber = function () {
   quizApp.surpriseNumber = Math.floor(Math.random() * 4);
   console.log(quizApp.surpriseNumber);
   return quizApp.surpriseNumber;
 };
 
+// Fetches random words
 quizApp.getRandomWord = function () {
   fetch(`https://random-word-api.herokuapp.com/word?number=4`)
     .then((res) => res.json())
@@ -85,6 +87,8 @@ quizApp.getRandomWord = function () {
       // buttonSubmit();
       //   selectedRadio(randomWord);
     });
+
+  // Fetches dictionary meaning for words
   quizApp.fetchDiction = (randomWord) => {
     fetch(
       `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${randomWord}?key=${quizApp.apiKEY}`
@@ -114,12 +118,11 @@ quizApp.resetAll = function () {
   quizApp.uncheckRadio();
 };
 
+// Loads quiz and core animations
 quizApp.loadQuiz = function () {
   quizApp.uncheckRadio();
   quizApp.getRandomWord();
   // selectedRadio()
-
-
   quizApp.liButtons.forEach(li => {
       li.classList.add('animateInto')
   })
@@ -129,18 +132,16 @@ quizApp.loadQuiz = function () {
   quizApp.liButtons.forEach((li) => {
     li.classList.remove("clicked");
   })
-  
-
-    
 };
 
+// Unchecks the radio options from memory
 quizApp.uncheckRadio = function () {
   quizApp.radioChoices.forEach((choice) => (choice.checked = false));
 };
 
+// Adds animations to radio buttons
 quizApp.liButtons.forEach((li) => {
   li.addEventListener("click", () => {
-    // console.log('heeeey')
     quizApp.liButtons.forEach(li => {
     li.classList.remove('animateInto')
     })
@@ -149,6 +150,7 @@ quizApp.liButtons.forEach((li) => {
   });
 });
 
+// Status bar functionality
 quizApp.statusCounter = 0;
 quizApp.setStatusBar = function () {
   quizApp.statusCounter++;
@@ -157,6 +159,7 @@ quizApp.setStatusBar = function () {
   document.querySelector(".statusBar").style.width = `${percentage}%`;
 };
 
+// Checks to see what button was selected
 quizApp.selectedRadio = function (randomWord) {
   let chosen;
   quizApp.radioChoices.forEach((choice) => {
@@ -167,8 +170,10 @@ quizApp.selectedRadio = function (randomWord) {
   return chosen;
 };
 
+// Answer submission button functionality and animations
 quizApp.submit.addEventListener("click", () => {
   quizApp.getrandomImage();
+  quizApp.illustration = document.querySelector('.illustration').innerHTML = " ";
   quizApp.displayImage();
   quizApp.changeColor();
   let chosen = quizApp.selectedRadio();
@@ -184,26 +189,30 @@ quizApp.submit.addEventListener("click", () => {
       quizApp.quizCard.innerHTML = `
            <h2>Congrats !!! 
            You got ${quizApp.scoreCounter}/ ${quizApp.gameLength} questions correctly</h2>
-           <button class="submit" onclick="location.reload()">Reload</button>`;
+           <button class="submit" onclick="location.reload()">Try Again</button>`;
     }
   }
 });
 
+// Generates random fun illustrations for the quiz card 
 quizApp.getrandomImage = function () {
   quizApp.randomImage = Math.floor(Math.random() * quizApp.imageSources.length);
   console.log(quizApp.randomImage, quizApp.imageSources[quizApp.randomImage]);
+  console.log('images')
   return quizApp.imageSources[quizApp.randomImage];
 };
 
+// Displays random illustrations
 quizApp.displayImage = function () {
-  quizApp.imageArea = document.createElement("div");
+  quizApp.illustration = document.querySelector('.illustration');
+  quizApp.illustration.style.display = 'block';
   quizApp.image = document.createElement("img");
   quizApp.image.src = `${quizApp.imageSources[quizApp.randomImage]}`;
-  quizApp.imageArea.appendChild(quizApp.image);
-  // document.querySelector(".imgContainer").innerHTML = " ";
+  quizApp.illustration.append(quizApp.image);
   // document.querySelector(".imgContainer").appendChild(quizApp.imageArea);
 };
 
+// IIFE to start things off after logging user name
 quizApp.init = function () {
   (() => {
     quizApp.resetAll();
